6544cfea8f73764661288cb40c13d747a94b099a
Sebastian Radynski-Figlarz <sebastianfiglarz+git@gmail.com>
2021-05-16 23:46:39 +0200

    Dodanie stabilnosci dzielenia zbioru wejsciowego na kilka czesci.
    Sparametryzowanie dzielenie-zbioru.R - dodanie nazwy pliku wyjsciowego (czyli plik z jedna czescia danych) jako parametru.

--- dzielenie-zbioru.R
+++ dzielenie-zbioru.R
@@ -1,19 +1,26 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-# ten skrypt przyjmuje dwa argumenty:
+# ten skrypt przyjmuje argumenty:
 # 1 sciezka do pliku z danymi wejsciowymi
 # 2 ilosc watkow (na ile czesci podzielic zbior danych wejsciowych)
+# 3 czesc nazwy pliku wyjsciowego (np zbior_ , ktore dalej bedzie uzupelniony w petli zeby bylo np. zbior_3.csv)
 # Jak przy wywolaniu skryptu nie podano pierwszego argumentu to rzucamy blad
-if (length(args) == 0) {
-  stop("Sciezka do pliku wejsciowego jest wymagana.", call. = FALSE)
+if (length(args)==0) {
+  stop("Sciezka do pliku wejsciowego jest wymagana.", call.=FALSE)
 }
 
+input_sciezka <- args[1]
+ilosc_zbiorow <- args[2]
+plik_output <- args[3]
+
+
+
 # wczytanie danych
-df <- read.table(args[1], sep = "", header = T)
+df <- read.table(input_sciezka, sep = "" , header = T)
 #df <- read.table("prostate_cancer.txt", sep = "" , header = T)
 
 licza_wierszy <- nrow(df)
-ilosc_zbiorow <- args[2]
+#ilosc_zbiorow = args[2] #zakomentowane bo ta linijka jest na gorze tego skryptu
 
 # trzeba dzielic zbior na mozliwie rowne czesci - ale nie od gory do dolu dzielac np. na 4,
 # tylko tak bardziej losowo, powyciagac wartosci z roznych wierszy i przyporzadkowac do roznych czesci
@@ -43,11 +50,12 @@ prop.table(table(numery_zbiorow))
 start.time <- Sys.time()
 
 
+
 # tworzy pusta liste - konstruktor utworzenia pustej listy
 lista_zbiorow <- list()
 
 
-for (numer in 1:ilosc_zbiorow) {
+for(numer in 1:ilosc_zbiorow){
   # which zwraca wektor indeksow
   # which zwraca dla podanego wektora indeksow numery tych rekordow, ktore odpowiadaja numerowi
   # (ktorych indeks czyli przypisanie do grupy pasuje do podanego == numer'u)
@@ -58,13 +66,13 @@ for (numer in 1:ilosc_zbiorow) {
   # potem jak mamy te indeksy, to wyciagamy odpowiednie wiersze i zapisujemy do listy
   # df [nr_wiersza, ] siegamy z ramki danych po wybrany wiersz, i po wszystkie kolumny
   # bo jak miejsce na kolumny jest puste to wybiera wartosci ze wszystkich kolumn
-  lista_zbiorow[[numer]] <- df[wybrane_idx,]
+  lista_zbiorow[[numer]] <- df[wybrane_idx, ]
 
   # potem ta liste zapisujemy do pliku
   # paste0 skleja string, normalnie jak w javie + konkatenacja
   # write.csv(df[wybrane_idx, ], paste0("zbior_", numer,".csv"), row.names = F)
 
-  write.csv(df[wybrane_idx,], paste0("Split-data\\zbior_", numer, ".csv"), row.names = F) # to dziala
+  write.csv(df[wybrane_idx, ], paste0(plik_output, numer,".csv"), row.names = F) # to dziala
   ?write.csv
   #write.table(df[wybrane_idx, ], file = paste0("zbior_", numer,".csv"),
   #append = TRUE, sep = "\t", row.names=FALSE, col.names=TRUE, quote=FALSE)
@@ -75,9 +83,9 @@ end.time <- Sys.time()
 time.taken <- as.numeric(end.time - start.time)
 time.taken <- format(round(time.taken, 2), nsmall = 2) # formatowanie do dwoch miejsc po przecinku
 cat("\n\n")
-print(paste0("Dzielenie zbioru danych wejsciowych zajelo: ", time.taken, " sekund(y)"))
-print(paste0("Start wykonania skryptu: ", start.time))
-print(paste0("Koniec wykonania skryptu: ", end.time))
+print(paste0("Dzielenie zbioru danych wejsciowych zajelo: ",time.taken, " sekund(y)"))
+print(paste0("Start wykonania skryptu: ",start.time))
+print(paste0("Koniec wykonania skryptu: ",end.time))
 
 #zamkniecie pliku
 sink()
