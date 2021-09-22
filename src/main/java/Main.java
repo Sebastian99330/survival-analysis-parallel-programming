94e976d59750e6301572d3870ebcc5704eb44063
Sebastian Radynski-Figlarz <sebastianfiglarz+git@gmail.com>
2021-09-22 17:22:57 +0200

    Poprawiona parametryzacja dla seq
    Next krok to parametryzacja parallel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
 import com.google.common.base.Stopwatch;
 
+import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         final Integer numberOfThreads = 5;
         final int numberOfFirstThread = 1;
+        String argsSeq [] = {"turnover.csv","output-seq.txt","km_seq.jpg","cph_seq.jpg","output_seq",
+                ",","ramka_seq.rds","exp, event","branch","branch + pipeline"}; // dla work
+//         String argsSeq [] = {"prostate_cancer.csv","output-seq.txt","km_seq.jpg","cph_seq.jpg","output_seq",
+//                ",","ramka_seq.rds","time, status","treatment","treatment + age + sh + size + index"}; // dla prostate cancerS
 
         TalkToR.clearWorkspace(numberOfThreads); // tworzy puste foldery na output (i ewentualnie usuwa istniejace)
 
         // sekwencyjnie
         Stopwatch timeSeq = Stopwatch.createStarted(); // pobranie czasu do mierzenia czasu wykonania algorytmu metoda sekwencyjna
-        SequentialAlgorithm.callRScript();
+        SequentialAlgorithm sequentialAlgorithm = new SequentialAlgorithm(argsSeq);
+        sequentialAlgorithm.callRScript();
         timeSeq.stop();
 
         //rownolegle
--- src/main/java/SequentialAlgorithm.java
+++ src/main/java/SequentialAlgorithm.java
@@ -1,29 +1,43 @@
 public class SequentialAlgorithm {
-    public static void callRScript(){
-        // argumenty dla sekwencyjnego algorytmu
-        String input = "turnover.csv";
-//        String input = "prostate_cancer.csv";
+    // argumenty dla sekwencyjnego algorytmu
+    String input = "turnover.csv";
+    //        String input = "prostate_cancer.csv";
 //        String input = "prost_cancer_mln.csv";
-        String outputTxtFile = "output-seq.txt";
-        String kphPlotPath = "km_seq.jpg";
-        String cphPlotPath = "cph_seq.jpg";
-        String outputFolderName = "output_seq";
-//        String rSeparator = "\"\""; // dla prostate cancer
-        String rSeparator = ",";     // dla work oraz prost cancer mln
-        // sekwencyjny wypisuje csv, bo to juz nie jest posredni output tylko ostateczny\
-        // jednak nie, musi byc rds bo potem rownolegle jeszcze to wczytuja
+    String outputTxtFile = "output-seq.txt";
+    String kphPlotPath = "km_seq.jpg";
+    String cphPlotPath = "cph_seq.jpg";
+    String outputFolderName = "output_seq";
+    //        String rSeparator = "\"\""; // dla prostate cancer
+    String rSeparator = ",";     // dla work oraz prost cancer mln
+    // sekwencyjny wypisuje csv, bo to juz nie jest posredni output tylko ostateczny\
+    // jednak nie, musi byc rds bo potem rownolegle jeszcze to wczytuja
 //        String dfName = "ramka_seq.csv";
-        String dfName = "ramka_seq.rds";
-//        String timeStatus = "time, status"; // dla input prostate cancer
+    String dfName = "ramka_seq.rds";
+    //        String timeStatus = "time, status"; // dla input prostate cancer
 //        String groupingVariablesKm = "treatment"; // dla input prostate cancer
 //        String groupingVariablesCox = "treatment + age + sh + size + index"; // dla input prostate cancer
-        String timeStatus = "exp, event"; // dla input work
-        String groupingVariablesKm = "branch"; // dla input work
-        String groupingVariablesCox = "branch + pipeline"; // dla input work
+    String timeStatus = "exp, event"; // dla input work
+    String groupingVariablesKm = "branch"; // dla input work
+    String groupingVariablesCox = "branch + pipeline"; // dla input work
 
-        String command = "rscript --vanilla script.r " + input + " " +
-                outputTxtFile + " " + kphPlotPath + " " + cphPlotPath + " " + outputFolderName + " " + rSeparator + " " +
-                dfName + " " + timeStatus + " " + groupingVariablesKm + " " + groupingVariablesCox;
+    public SequentialAlgorithm(String[] args) {
+        this.input = args[0];
+        this.outputTxtFile = args[1];
+        this.kphPlotPath = args[2];
+        this.cphPlotPath = args[3];
+        this.outputFolderName = args[4];
+        this.rSeparator = args[5];
+        this.dfName = args[6];
+        this.timeStatus = args[7];
+        this.groupingVariablesKm = args[8];
+        this.groupingVariablesCox = args[9];
+    }
+
+    public void callRScript(){
+
+        String command = "rscript --vanilla script.r " + this.input + " " +
+                this.outputTxtFile + " " + this.kphPlotPath + " " + this.cphPlotPath + " " + this.outputFolderName + " " + this.rSeparator + " " +
+                this.dfName + " " + this.timeStatus + " " + this.groupingVariablesKm + " " + this.groupingVariablesCox;
 
         TalkToR.runScript(command, false);
     }
