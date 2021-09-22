8a63e41923bd853bffdb6efe0912c677c3def94e
Sebastian Radynski-Figlarz <sebastianfiglarz+git@gmail.com>
2021-09-22 17:24:41 +0200

    Poprawiona parametryzacja dla seq
    Next krok to parametryzacja parallel

--- src/main/java/SequentialAlgorithm.java
+++ src/main/java/SequentialAlgorithm.java
@@ -1,24 +1,15 @@
 public class SequentialAlgorithm {
     // argumenty dla sekwencyjnego algorytmu
-    String input = "turnover.csv";
-    //        String input = "prostate_cancer.csv";
-//        String input = "prost_cancer_mln.csv";
-    String outputTxtFile = "output-seq.txt";
-    String kphPlotPath = "km_seq.jpg";
-    String cphPlotPath = "cph_seq.jpg";
-    String outputFolderName = "output_seq";
-    //        String rSeparator = "\"\""; // dla prostate cancer
-    String rSeparator = ",";     // dla work oraz prost cancer mln
-    // sekwencyjny wypisuje csv, bo to juz nie jest posredni output tylko ostateczny\
-    // jednak nie, musi byc rds bo potem rownolegle jeszcze to wczytuja
-//        String dfName = "ramka_seq.csv";
-    String dfName = "ramka_seq.rds";
-    //        String timeStatus = "time, status"; // dla input prostate cancer
-//        String groupingVariablesKm = "treatment"; // dla input prostate cancer
-//        String groupingVariablesCox = "treatment + age + sh + size + index"; // dla input prostate cancer
-    String timeStatus = "exp, event"; // dla input work
-    String groupingVariablesKm = "branch"; // dla input work
-    String groupingVariablesCox = "branch + pipeline"; // dla input work
+    String input;
+    String outputTxtFile;
+    String kphPlotPath;
+    String cphPlotPath;
+    String outputFolderName;
+    String rSeparator;
+    String dfName;
+    String timeStatus;
+    String groupingVariablesKm;
+    String groupingVariablesCox;
 
     public SequentialAlgorithm(String[] args) {
         this.input = args[0];
